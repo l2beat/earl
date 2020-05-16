701226aa117fd84a00a318f33161a9df12423052
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-05-16 11:23:32 +0200

    Implement mocks (#17)

---
+++ src/Mock.ts
@@ -0,0 +1,239 @@
+import { smartEq } from './matchers/toEqual'
+
+export interface MockCall {
+  args: any[]
+  result: { type: 'return'; value: any } | { type: 'throw'; error: any }
+}
+
+export interface Mock<A extends any[], T> {
+  /** Calls the mock function */
+  (...args: A): T
+  calls: MockCall[]
+  isExhausted(): boolean
+  /**
+   * Sets the return value of calls to the Mock.
+   * Overrides any previous configuration.
+   * @param value value to be returned.
+   */
+  returns<U>(value: U): Mock<any[], U>
+  /**
+   * Schedules the mock to return a value the next time it's called.
+   * If anything is already scheduled it will be used first.
+   * @param value value to be returned.
+   */
+  returnsOnce<U>(value: U): Mock<A, T | U>
+  /**
+   * Sets the error thrown by calls to the Mock.
+   * Overrides any previous configuration.
+   * @param error error to be thrown.
+   */
+  throws(error: any): Mock<any[], never>
+  /**
+   * Schedules the mock to throw an error the next time it's called.
+   * If anything is already scheduled it will be used first.
+   * @param error error to be thrown.
+   */
+  throwsOnce(error: any): Mock<A, T>
+  /**
+   * Sets the underlying implementation of the Mock.
+   * Overrides any previous configuration.
+   * @param implementation function to execute.
+   */
+  executes<B extends any[], U>(implementation: (...args: B) => U): Mock<B, U>
+  /**
+   * Schedules the mock use the provided implementation the next time it's called.
+   * If anything is already scheduled it will be used first.
+   * @param implementation function to execute.
+   */
+  executesOnce<B extends A, U>(implementation: (...args: B) => U): Mock<B, T | U>
+  /**
+   * Specifies a different behavior when other arguments are given
+   * @param args arguments to match
+   */
+  given<B extends A>(
+    ...args: B
+  ): {
+    /**
+     * Sets the return value of calls to the Mock.
+     * @param value value to be returned.
+     */
+    returns<U>(value: U): Mock<A, T | U>
+    /**
+     * Schedules the mock to return a value the next time it's called.
+     * If anything is already scheduled it will be used first.
+     * @param value value to be returned.
+     */
+    returnsOnce<U>(value: U): Mock<A, T | U>
+    /**
+     * Sets the error thrown by calls to the Mock.
+     * @param error error to be thrown.
+     */
+    throws(error: any): Mock<A, T>
+    /**
+     * Schedules the mock to throw an error the next time it's called.
+     * If anything is already scheduled it will be used first.
+     * @param error error to be thrown.
+     */
+    throwsOnce(error: any): Mock<A, T>
+    /**
+     * Sets the underlying implementation of the Mock.
+     * @param implementation function to execute.
+     */
+    executes<U>(implementation: (...args: B) => U): Mock<A, T | U>
+    /**
+     * Schedules the mock use the provided implementation the next time it's called.
+     * If anything is already scheduled it will be used first.
+     * @param implementation function to execute.
+     */
+    executesOnce<U>(implementation: (...args: B) => U): Mock<A, T | U>
+  }
+}
+
+interface ReturnSpec {
+  type: 'return'
+  value: any
+}
+
+interface ThrowSpec {
+  type: 'throw'
+  error: any
+}
+
+interface ExecSpec {
+  type: 'exec'
+  implementation: (...args: any[]) => any
+}
+
+type Spec = ReturnSpec | ThrowSpec | ExecSpec
+
+interface Override {
+  args: any[]
+  spec: Spec
+}
+
+export function mockFn(): Mock<any[], undefined> {
+  let spec: Spec = {
+    type: 'return',
+    value: undefined,
+  }
+  let queue: Spec[] = []
+  let oneTimeOverrides: Override[] = []
+  let recurringOverrides: Override[] = []
+
+  function mock(...args: any[]) {
+    for (const override of oneTimeOverrides) {
+      if (smartEq(args, override.args)) {
+        oneTimeOverrides.splice(oneTimeOverrides.indexOf(override), 1)
+        return runSpec(override.spec, args)
+      }
+    }
+    for (const override of recurringOverrides) {
+      if (smartEq(args, override.args)) {
+        return runSpec(override.spec, args)
+      }
+    }
+    const current = queue.shift() || spec
+    return runSpec(current, args)
+  }
+
+  mock.calls = [] as MockCall[]
+  mock.isExhausted = function () {
+    return queue.length === 0 && oneTimeOverrides.length === 0
+  }
+
+  function runSpec(spec: Spec, args: any[]) {
+    switch (spec.type) {
+      case 'return':
+        mock.calls.push({ args, result: { type: 'return', value: spec.value } })
+        return spec.value
+      case 'throw':
+        mock.calls.push({ args, result: { type: 'throw', error: spec.error } })
+        throw spec.error
+      case 'exec':
+        try {
+          const value = spec.implementation(...args)
+          mock.calls.push({ args, result: { type: 'return', value } })
+          return value
+        } catch (error) {
+          mock.calls.push({ args, result: { type: 'throw', error } })
+          throw error
+        }
+    }
+  }
+
+  function reset(newSpec: Spec) {
+    spec = newSpec
+    queue = []
+    oneTimeOverrides = []
+    recurringOverrides = []
+  }
+
+  mock.returns = function (value: any) {
+    reset({ type: 'return', value })
+    return mock
+  }
+
+  mock.returnsOnce = function (value: any) {
+    queue.push({ type: 'return', value })
+    return mock
+  }
+
+  mock.throws = function (error: any) {
+    reset({ type: 'throw', error })
+    return mock
+  }
+
+  mock.throwsOnce = function (error: any) {
+    queue.push({ type: 'throw', error })
+    return mock
+  }
+
+  mock.executes = function (implementation: (...args: any[]) => any) {
+    reset({ type: 'exec', implementation })
+    return mock
+  }
+
+  mock.executesOnce = function (implementation: (...args: any[]) => any) {
+    queue.push({ type: 'exec', implementation })
+    return mock
+  }
+
+  mock.given = function (...args: any[]) {
+    return {
+      returns(value: any) {
+        recurringOverrides.push({ args, spec: { type: 'return', value } })
+        return mock
+      },
+
+      returnsOnce(value: any) {
+        oneTimeOverrides.push({ args, spec: { type: 'return', value } })
+        return mock
+      },
+
+      throws(error: any) {
+        recurringOverrides.push({ args, spec: { type: 'throw', error } })
+        return mock
+      },
+
+      throwsOnce(error: any) {
+        oneTimeOverrides.push({ args, spec: { type: 'throw', error } })
+        return mock
+      },
+
+      executes(implementation: (...args: any[]) => any) {
+        recurringOverrides.push({
+          args,
+          spec: { type: 'exec', implementation },
+        })
+        return mock
+      },
+
+      executesOnce(implementation: (...args: any[]) => any) {
+        oneTimeOverrides.push({ args, spec: { type: 'exec', implementation } })
+        return mock
+      },
+    }
+  }
+
+  return mock
+}
---
+++ test/Mock.test.ts
@@ -0,0 +1,316 @@
+import { expect } from 'chai'
+
+import { expect as earl } from '../src'
+import { mockFn } from '../src/Mock'
+
+describe('Mock', () => {
+  describe('mockFn()', () => {
+    it('creates a function', () => {
+      const fn = mockFn()
+      expect(fn).to.be.instanceOf(Function)
+    })
+
+    it('function returns undefined by default', () => {
+      const fn = mockFn()
+      expect(fn()).to.equal(undefined)
+    })
+  })
+
+  describe('.returns', () => {
+    it('sets function return value', () => {
+      const fn = mockFn().returns(3)
+      expect(fn()).to.equal(3)
+    })
+
+    it('resets any existing config', () => {
+      const fn = mockFn()
+        .throws(new Error('Boom'))
+        .given(1, 2)
+        .executes((a, b) => a + b)
+        .returnsOnce(55)
+        .returns(3)
+
+      expect(fn()).to.equal(3)
+      expect(fn(1, 2)).to.equal(3)
+    })
+  })
+
+  describe('.returnsOnce', () => {
+    it('queues function return value', () => {
+      const fn = mockFn().returnsOnce(3)
+      expect(fn()).to.equal(3)
+      expect(fn()).to.equal(undefined)
+    })
+
+    it('can queue multiple values', () => {
+      const fn = mockFn().returnsOnce(3).returnsOnce(4).returnsOnce('hello')
+      expect(fn()).to.equal(3)
+      expect(fn()).to.equal(4)
+      expect(fn()).to.equal('hello')
+      expect(fn()).to.equal(undefined)
+    })
+
+    it('respects previous configuration', () => {
+      const fn = mockFn()
+        .executes((x: number) => x + 1)
+        .returnsOnce(3)
+      expect(fn(1)).to.equal(3)
+      expect(fn(1)).to.equal(2)
+    })
+  })
+
+  describe('.throws', () => {
+    it('sets function to throw', () => {
+      const fn = mockFn().throws(new Error('Boom'))
+      expect(fn).to.throw('Boom')
+    })
+
+    it('resets any existing config', () => {
+      const fn = mockFn()
+        .returns(3)
+        .given(1, 2)
+        .executes((a, b) => a + b)
+        .returnsOnce(55)
+        .throws(new Error('Boom'))
+
+      expect(fn).to.throw('Boom')
+      expect(() => fn(1, 2)).to.throw('Boom')
+    })
+  })
+
+  describe('.throwsOnce', () => {
+    it('queues function to throw', () => {
+      const fn = mockFn().throwsOnce(new Error('Boom'))
+      expect(fn).to.throw('Boom')
+      expect(fn()).to.equal(undefined)
+    })
+
+    it('can queue multiple values', () => {
+      const fn = mockFn().throwsOnce(new Error('Boom')).throwsOnce(new Error('Bam')).throwsOnce(new TypeError('BANG'))
+      expect(fn).to.throw(Error, 'Boom')
+      expect(fn).to.throw(Error, 'Bam')
+      expect(fn).to.throw(TypeError, 'BANG')
+      expect(fn()).to.equal(undefined)
+    })
+
+    it('respects previous configuration', () => {
+      const fn = mockFn()
+        .executes((x: number) => x + 1)
+        .throwsOnce(new Error('Boom'))
+      expect(fn).to.throw('Boom')
+      expect(fn(1)).to.equal(2)
+    })
+  })
+
+  describe('.executes', () => {
+    it('sets function to execute implementation', () => {
+      const fn = mockFn().executes((x: number) => x + 1)
+      expect(fn(4)).to.equal(5)
+    })
+
+    it('resets any existing config', () => {
+      const fn = mockFn()
+        .returns(3)
+        .given('foo')
+        .returns(5)
+        .returnsOnce(55)
+        .executes((x: string) => 'Hey ' + x)
+
+      expect(fn('Marie')).to.equal('Hey Marie')
+      expect(fn('foo')).to.equal('Hey foo')
+    })
+  })
+
+  describe('.executesOnce', () => {
+    it('queues function to execute implementation', () => {
+      const fn = mockFn().executesOnce((x: number) => x + 1)
+      expect(fn(4)).to.equal(5)
+      expect(fn(4)).to.equal(undefined)
+    })
+
+    it('can queue multiple values', () => {
+      const fn = mockFn()
+        .executesOnce((x: number) => x + 1)
+        .executesOnce((x: number) => x / 2)
+        .executesOnce(() => 15)
+      expect(fn(4)).to.equal(5)
+      expect(fn(4)).to.equal(2)
+      expect(fn(4)).to.equal(15)
+      expect(fn(4)).to.equal(undefined)
+    })
+
+    it('respects previous configuration', () => {
+      const fn = mockFn()
+        .executes((x: number) => x + 1)
+        .executesOnce((x: number) => x / 2)
+      expect(fn(4)).to.equal(2)
+      expect(fn(4)).to.equal(5)
+    })
+  })
+
+  describe('.given', () => {
+    it('supports .returns', () => {
+      const fn = mockFn().given(1, 2).returns(3)
+      expect(fn(1, 2)).to.equal(3)
+      expect(fn(1, 2)).to.equal(3)
+      expect(fn(3, 4)).to.equal(undefined)
+      expect(fn()).to.equal(undefined)
+    })
+
+    it('supports .returnsOnce', () => {
+      const fn = mockFn().given(1, 2).returnsOnce(3)
+      expect(fn(1, 2)).to.equal(3)
+      expect(fn(1, 2)).to.equal(undefined)
+      expect(fn(3, 4)).to.equal(undefined)
+      expect(fn()).to.equal(undefined)
+    })
+
+    it('supports multiple .returnsOnce', () => {
+      const fn = mockFn().given(1, 2).returnsOnce(3).given(1, 2).returnsOnce(4)
+      expect(fn(1, 2)).to.equal(3)
+      expect(fn(1, 2)).to.equal(4)
+      expect(fn(1, 2)).to.equal(undefined)
+      expect(fn(3, 4)).to.equal(undefined)
+      expect(fn()).to.equal(undefined)
+    })
+
+    it('supports .throws', () => {
+      const fn = mockFn().given(1, 2).throws(new Error('Boom'))
+      expect(() => fn(1, 2)).to.throw('Boom')
+      expect(() => fn(1, 2)).to.throw('Boom')
+      expect(fn(3, 4)).to.equal(undefined)
+      expect(fn()).to.equal(undefined)
+    })
+
+    it('supports .throwsOnce', () => {
+      const fn = mockFn().given(1, 2).throwsOnce(new Error('Boom'))
+      expect(() => fn(1, 2)).to.throw('Boom')
+      expect(fn(1, 2)).to.equal(undefined)
+      expect(fn(3, 4)).to.equal(undefined)
+      expect(fn()).to.equal(undefined)
+    })
+
+    it('supports multiple .throwsOnce', () => {
+      const fn = mockFn().given(1, 2).throwsOnce(new Error('Boom')).given(1, 2).throwsOnce(new Error('Bam'))
+      expect(() => fn(1, 2)).to.throw('Boom')
+      expect(() => fn(1, 2)).to.throw('Bam')
+      expect(fn(1, 2)).to.equal(undefined)
+      expect(fn(3, 4)).to.equal(undefined)
+      expect(fn()).to.equal(undefined)
+    })
+
+    it('supports .executes', () => {
+      const fn = mockFn()
+        .given(1, 2)
+        .executes((a, b) => a + b)
+      expect(fn(1, 2)).to.equal(3)
+      expect(fn(1, 2)).to.equal(3)
+      expect(fn(3, 4)).to.equal(undefined)
+    })
+
+    it('supports .executesOnce', () => {
+      const fn = mockFn()
+        .given(1, 2)
+        .executesOnce((a, b) => a + b)
+      expect(fn(1, 2)).to.equal(3)
+      expect(fn(1, 2)).to.equal(undefined)
+      expect(fn(3, 4)).to.equal(undefined)
+    })
+
+    it('supports multiple .executesOnce', () => {
+      const fn = mockFn()
+        .given(1, 2)
+        .executesOnce((a, b) => a + b)
+        .given(1, 2)
+        .executesOnce((a, b) => a + b * 2)
+      expect(fn(1, 2)).to.equal(3)
+      expect(fn(1, 2)).to.equal(5)
+      expect(fn(1, 2)).to.equal(undefined)
+      expect(fn(3, 4)).to.equal(undefined)
+    })
+
+    it('supports asymmetric matchers', () => {
+      const fn = mockFn().returns(null).given(earl.a(Number)).returns(3).given(earl.a(String)).returns('yes')
+
+      expect(fn(false)).to.equal(null)
+      expect(fn(1)).to.equal(3)
+      expect(fn('foo')).to.equal('yes')
+    })
+  })
+
+  describe('.calls', () => {
+    it('is empty at first', () => {
+      const fn = mockFn()
+      expect(fn.calls).to.deep.equal([])
+    })
+
+    it('stores a single call', () => {
+      const fn = mockFn()
+      fn()
+      expect(fn.calls).to.deep.equal([{ args: [], result: { type: 'return', value: undefined } }])
+    })
+
+    it('stores multiple calls', () => {
+      const fn = mockFn()
+      fn()
+      fn(1)
+      fn(5, 'yo')
+      expect(fn.calls).to.deep.equal([
+        { args: [], result: { type: 'return', value: undefined } },
+        { args: [1], result: { type: 'return', value: undefined } },
+        { args: [5, 'yo'], result: { type: 'return', value: undefined } },
+      ])
+    })
+
+    it('respects .throws', () => {
+      const error = new Error('Boom')
+      const fn = mockFn().throws(error)
+      try {
+        fn()
+      } catch {}
+      expect(fn.calls).to.deep.equal([{ args: [], result: { type: 'throw', error } }])
+    })
+
+    it('respects .executes', () => {
+      const error = new Error('Boom')
+      const fn = mockFn().executes((x: number) => {
+        if (x < 3) {
+          return 3
+        } else {
+          throw error
+        }
+      })
+      try {
+        fn(2)
+        fn(5)
+      } catch {}
+      expect(fn.calls).to.deep.equal([
+        { args: [2], result: { type: 'return', value: 3 } },
+        { args: [5], result: { type: 'throw', error } },
+      ])
+    })
+  })
+
+  describe('.isExhausted', () => {
+    it('returns true initially', () => {
+      const fn = mockFn()
+      expect(fn.isExhausted()).to.equal(true)
+    })
+
+    it('returns false if there are queued calls', () => {
+      const fn = mockFn().returnsOnce(3)
+      expect(fn.isExhausted()).to.equal(false)
+      fn()
+      expect(fn.isExhausted()).to.equal(true)
+    })
+
+    it('returns false if there are queued calls with argument matching', () => {
+      const fn = mockFn().given(1).returnsOnce(3)
+      expect(fn.isExhausted()).to.equal(false)
+      fn()
+      expect(fn.isExhausted()).to.equal(false)
+      fn(1)
+      expect(fn.isExhausted()).to.equal(true)
+    })
+  })
+})
