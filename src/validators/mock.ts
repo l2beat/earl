ad76f878bfef1b39013ca6428d416bae0270e061
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-05-20 19:29:27 +0200

    Don't return control.assert

--- src/validators/mock.ts
+++ src/validators/mock.ts
@@ -2,7 +2,7 @@ import { Mock } from '../mocks/common'
 import { Control } from './common'
 
 export function toBeExhausted(control: Control<Mock>) {
-  return control.assert({
+  control.assert({
     success: control.actual.isExhausted(),
     reason: 'Mock not exhausted!',
     negatedReason: 'Mock exhausted!',
