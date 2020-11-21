00411e6102149d42a4c343add4393a2f4c2a0244
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-11-21 13:28:14 +0100

    Re-add formatting to docs

---
+++ packages/earl/.mocharc.js
@@ -0,0 +1,16 @@
+// ensure NODE_ENV
+process.env.NODE_ENV = 'test'
+
+// exit test runner on unhandled rejections
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection during test execution:', promise, 'reason:', reason)
+  process.exit(1)
+})
+
+module.exports = {
+  require: 'ts-node/register/transpile-only',
+  file: 'test/setup.ts',
+  extension: 'ts',
+  watchExtensions: 'ts',
+  spec: 'test/**/*.test.ts',
+}
