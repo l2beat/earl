a6e83f4bb775817601d6aee946b57a56f32aa959
grzpab <grzpab@users.noreply.github.com>
2020-11-21 20:45:49 +0100

    add tests for the ArrayWithMatcher

---
+++ packages/earljs/test/matchers/ArrayWith.test.ts
@@ -0,0 +1,55 @@
+import { expect } from 'chai'
+
+import { expect as earlExpect } from '../../src'
+import { ArrayWithMatcher } from '../../src/matchers'
+
+describe('ArrayWith matcher', () => {
+  it('matches a single-element array', () => {
+    const m = new ArrayWithMatcher([1])
+
+    expect(m.check([])).to.be.false
+    expect(m.check(['a'])).to.be.false
+    expect(m.check([1])).to.be.true
+    expect(m.check([1, 2, 3])).to.be.true
+  })
+
+  it('matches a multi-element array', () => {
+    const m = new ArrayWithMatcher([1, 2, 3])
+
+    expect(m.check([])).to.be.false
+    expect(m.check(['a'])).to.be.false
+    expect(m.check([1])).to.be.true
+    expect(m.check([1, 2, 3])).to.be.true
+    expect(m.check([1, 2, 3, 4, 5, 6])).to.be.true
+  })
+
+  it("doesn't match non iterables", () => {
+    const m = new ArrayWithMatcher([1])
+
+    expect(m.check({ something: 1 })).to.be.false
+    expect(m.check(null)).to.be.false
+    expect(m.check(undefined)).to.be.false
+    expect(m.check(1)).to.be.false
+  })
+
+  describe('in expectation', () => {
+    it('works', () => {
+      earlExpect([1, 2, 3]).toEqual(earlExpect.arrayWith(3))
+    })
+
+    it('works with nested matchers', () => {
+      earlExpect({ arr: [1, 5, 10] }).toEqual({
+        arr: earlExpect.arrayWith(earlExpect.numberCloseTo(6, { delta: 1 })),
+      })
+    })
+
+    it('throws understandable error messages', () => {
+      expect(() =>
+        earlExpect({ arr: [1, 2, 3] }).toEqual({
+          arr: earlExpect.arrayWith(earlExpect.numberCloseTo(6, { delta: 1 })),
+        }),
+      ).to.throw(`{"arr": [1, 2, 3]} not equal to {"arr": "[ArrayWith: [NumberCloseTo: 6, delta=1]]"}
+Hint: value mismatch`)
+    })
+  })
+})
