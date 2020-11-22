b51314ce6fada66fc640b4f223d97dbbee8bfd0b
grzpab <grzpab@users.noreply.github.com>
2020-11-22 13:01:00 +0100

    ensure ContainerWith::make allows an varargs

--- packages/earljs/src/matchers/ContainerWith.ts
+++ packages/earljs/src/matchers/ContainerWith.ts
@@ -32,7 +32,7 @@ export class ContainerWithMatcher<T> extends Matcher {
   // if we type it as T[] it's fine but then it won't work with iterators
   // if we type is as IterableIterator<T> then it is not assignable to T[] anymore :/
   // this is a problem for all polymorphic matchers
-  static make(item: any): any {
-    return new ContainerWithMatcher(item) as any
+  static make(...items: any[]): any[] {
+    return new ContainerWithMatcher(items) as any
   }
 }
