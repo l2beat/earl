fbdf4b41c897eedb92dcc99ad4602a1b635d15a8
grzpab <grzpab@users.noreply.github.com>
2020-11-22 14:05:39 +0100

    introduce ObjectWithMatcher

---
+++ packages/earljs/src/matchers/ObjectWith.ts
@@ -0,0 +1,33 @@
+import { smartEq } from '../validators/smartEq'
+import { Matcher } from './Base'
+
+/**
+ * Matches an object containing given key-value pairs.
+ */
+export class ObjectWithMatcher<T> extends Matcher {
+  constructor(private readonly expectedItem: T) {
+    super()
+  }
+
+  check(actualItem: unknown): boolean {
+    if (typeof actualItem !== 'object' || actualItem === null) {
+      return false
+    }
+
+    const expectedEntries = Object.entries(this.expectedItem)
+
+    return expectedEntries.every(([expectedKey, expectedValue]) => {
+      const actualValue = (actualItem as any)[expectedKey]
+
+      return smartEq(actualValue, expectedValue).result === 'success'
+    })
+  }
+
+  toString() {
+    return `[ObjectWith: ${this.expectedItem}]`
+  }
+
+  static make<T>(expectedItem: T): T {
+    return new ObjectWithMatcher(expectedItem) as any
+  }
+}
