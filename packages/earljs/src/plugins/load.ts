228a7c70b660c0dda2f6aa79a7e4306477d4e439
Krzysztof Kaczor <krzkaczor@gmail.com>
2020-11-28 19:28:14 +0100

    Cleanup debug logs for plugins

--- packages/earljs/src/plugins/load.ts
+++ packages/earljs/src/plugins/load.ts
@@ -10,20 +10,29 @@ import { loadSmartEqRules } from '../validators/smartEq'
 export type PluginLoader = (path: string) => Promise<PluginConfig>
 
 export function loadPlugin(pluginConfig: Partial<PluginConfig>): void {
-  logger(`Loading plugin: ${JSON.stringify(pluginConfig)}`)
+  logger(`Loading plugin: ${getPluginSummary(pluginConfig)}`)
 
   if (pluginConfig.matchers) {
-    logger(`Loading matchers: ${pluginConfig.matchers.length}`)
+    logger(`Loading matchers...`)
     loadMatchers(pluginConfig.matchers)
   }
 
   if (pluginConfig.validators) {
-    logger(`Loading validators: ${pluginConfig.validators.length}`)
+    logger(`Loading validators...`)
     loadValidators(pluginConfig.validators)
   }
 
   if (pluginConfig.smartEqRules) {
-    logger(`Loading smart eq rules: ${pluginConfig.smartEqRules.length}`)
+    logger(`Loading smart eq rules...`)
     loadSmartEqRules(pluginConfig.smartEqRules)
   }
+}
+
+function getPluginSummary(pluginConfig: Partial<PluginConfig>): string {
+  let out = ''
+  out += `Matchers: #${pluginConfig.matchers ? Object.keys(pluginConfig.matchers).length : 0} `
+  out += `Validators: #${pluginConfig.validators ? Object.keys(pluginConfig.validators).length : 0} `
+  out += `SmartEqRules: #${pluginConfig.smartEqRules?.length || 0} `
+
+  return out
 }
