27b5e0cd09d5f03b9f43d58bc02cdff5876e70e3
Krzysztof Kaczor <krzkaczor@gmail.com>
2020-12-06 16:14:16 +0100

    Tweak tests to fail

--- packages/earljs/test/validators/mocks/toHaveBeenCalledExactlyWith.test.ts
+++ packages/earljs/test/validators/mocks/toHaveBeenCalledExactlyWith.test.ts
@@ -2,36 +2,37 @@ import { expect } from 'chai'
 
 import { expect as earl } from '../../../src'
 import { mockFn } from '../../../src/mocks'
-import { noop } from '../../common'
+
+const sum = (a: number, b: number) => a + b
 
 describe('toHaveBeenCalledExactlyWith', () => {
   describe('not negated', () => {
     it('works when was called with a given args', () => {
-      const mock = mockFn(noop)
+      const mock = mockFn(sum)
 
-      mock(1, 2, 3)
+      mock(1, 2)
 
-      expect(() => earl(mock).toHaveBeenCalledExactlyWith([[1, 2, 3]])).not.to.throw()
+      expect(() => earl(mock).toHaveBeenCalledExactlyWith([[1, 2]])).not.to.throw()
     })
 
     it('works when was not called at all', () => {
-      const mock = mockFn(noop)
+      const mock = mockFn(sum)
 
       expect(() => earl(mock).toHaveBeenCalledExactlyWith([])).not.to.throw()
     })
 
     it('works with matchers', () => {
-      const mock = mockFn(noop)
+      const mock = mockFn(sum)
 
-      mock(1, 2, 3)
+      mock(1, 2)
 
       expect(() => earl(mock).toHaveBeenCalledExactlyWith([[1, 2, earl.a(Number)]])).not.to.throw()
     })
 
     it('throws on partial matches', () => {
-      const mock = mockFn(noop)
+      const mock = mockFn(sum)
 
-      mock(1, 2, 3)
+      mock(1, 2)
 
       expect(() => earl(mock).toHaveBeenCalledExactlyWith([[1, 2]])).to.throw(
         'Mock was not called exactly with [[1, 2]] but was expected to',
@@ -41,9 +42,9 @@ describe('toHaveBeenCalledExactlyWith', () => {
 
   describe('negated', () => {
     it('throws when was called with a given args', () => {
-      const mock = mockFn(noop)
+      const mock = mockFn(sum)
 
-      mock(1, 2, 3)
+      mock(1, 2)
 
       expect(() => earl(mock).not.toHaveBeenCalledExactlyWith([[1, 2, 3]])).to.throw(
         "Mock was called exactly with [[1, 2, 3]] but wasn't expected to",
@@ -51,15 +52,15 @@ describe('toHaveBeenCalledExactlyWith', () => {
     })
 
     it("works when wasn't called with expected args", () => {
-      const mock = mockFn(noop)
+      const mock = mockFn(sum)
 
-      mock(1, 2, 3)
+      mock(1, 2)
 
       expect(() => earl(mock).not.toHaveBeenCalledExactlyWith([[1, 2]])).not.to.throw()
     })
 
     it('works with empty mocks', () => {
-      const mock = mockFn(noop)
+      const mock = mockFn(sum)
 
       expect(() => earl(mock).not.toHaveBeenCalledExactlyWith([])).to.throw(
         "Mock was called exactly with [] but wasn't expected to",
