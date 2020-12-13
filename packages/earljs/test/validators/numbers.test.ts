15ade00d6395b912737d637bfa2e5622987ddb06
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-12-13 22:38:42 +0100

    Add number matchers

--- packages/earljs/test/validators/numbers.test.ts
+++ packages/earljs/test/validators/numbers.test.ts
@@ -2,17 +2,17 @@ import { expect } from 'chai'
 
 import { expect as earl } from '../../src'
 
-describe('number matchers', () => {
+describe('number validators', () => {
   describe('toBeGreaterThan', () => {
     describe('normal', () => {
       it('succeeds for a greater number', () => {
         expect(() => earl(2).toBeGreaterThan(1)).not.to.throw()
       })
-  
+
       it('fails for an equal number', () => {
         expect(() => earl(3).toBeGreaterThan(3)).to.throw('3 is not greater than 3')
       })
-  
+
       it('fails for a lesser number', () => {
         expect(() => earl(-2).toBeGreaterThan(4)).to.throw('-2 is not greater than 4')
       })
@@ -22,11 +22,11 @@ describe('number matchers', () => {
       it('fails for a greater number', () => {
         expect(() => earl(2).not.toBeGreaterThan(1)).to.throw('2 is greater than 1')
       })
-  
+
       it('succeeds for an equal number', () => {
         expect(() => earl(3).not.toBeGreaterThan(3)).not.to.throw()
       })
-  
+
       it('succeeds for a lesser number', () => {
         expect(() => earl(-2).not.toBeGreaterThan(4)).not.to.throw()
       })
@@ -38,11 +38,11 @@ describe('number matchers', () => {
       it('succeeds for a greater number', () => {
         expect(() => earl(2).toBeGreaterThanOrEqualTo(1)).not.to.throw()
       })
-  
+
       it('succeeds for an equal number', () => {
         expect(() => earl(3).toBeGreaterThanOrEqualTo(3)).not.to.throw()
       })
-  
+
       it('fails for a lesser number', () => {
         expect(() => earl(-2).toBeGreaterThanOrEqualTo(4)).to.throw('-2 is not greater than or equal to 4')
       })
@@ -52,11 +52,11 @@ describe('number matchers', () => {
       it('fails for a greater number', () => {
         expect(() => earl(2).not.toBeGreaterThanOrEqualTo(1)).to.throw('2 is greater than or equal to 1')
       })
-  
+
       it('fails for an equal number', () => {
         expect(() => earl(3).not.toBeGreaterThanOrEqualTo(3)).to.throw('3 is greater than or equal to 3')
       })
-  
+
       it('succeeds for a lesser number', () => {
         expect(() => earl(-2).not.toBeGreaterThanOrEqualTo(4)).not.to.throw()
       })
@@ -68,11 +68,11 @@ describe('number matchers', () => {
       it('fails for a greater number', () => {
         expect(() => earl(2).toBeLessThan(1)).to.throw('2 is not less than 1')
       })
-  
+
       it('fails for an equal number', () => {
         expect(() => earl(3).toBeLessThan(3)).to.throw('3 is not less than 3')
       })
-  
+
       it('succeeds for a lesser number', () => {
         expect(() => earl(-2).toBeLessThan(4)).not.to.throw()
       })
@@ -82,11 +82,11 @@ describe('number matchers', () => {
       it('succeeds for a greater number', () => {
         expect(() => earl(2).not.toBeLessThan(1)).not.to.throw()
       })
-  
+
       it('succeeds for an equal number', () => {
         expect(() => earl(3).not.toBeLessThan(3)).not.to.throw()
       })
-  
+
       it('fails for a lesser number', () => {
         expect(() => earl(-2).not.toBeLessThan(4)).to.throw('-2 is less than 4')
       })
@@ -98,11 +98,11 @@ describe('number matchers', () => {
       it('fails for a greater number', () => {
         expect(() => earl(2).toBeLessThanOrEqualTo(1)).to.throw('2 is not less than or equal to 1')
       })
-  
+
       it('succeeds for an equal number', () => {
         expect(() => earl(3).toBeLessThanOrEqualTo(3)).not.to.throw()
       })
-  
+
       it('succeeds for a lesser number', () => {
         expect(() => earl(-2).toBeLessThanOrEqualTo(4)).not.to.throw()
       })
@@ -112,11 +112,11 @@ describe('number matchers', () => {
       it('succeeds for a greater number', () => {
         expect(() => earl(2).not.toBeLessThanOrEqualTo(1)).not.to.throw()
       })
-  
+
       it('fails for an equal number', () => {
         expect(() => earl(3).not.toBeLessThanOrEqualTo(3)).to.throw('3 is less than or equal to 3')
       })
-  
+
       it('fails for a lesser number', () => {
         expect(() => earl(-2).not.toBeLessThanOrEqualTo(4)).to.throw('-2 is less than or equal to 4')
       })
