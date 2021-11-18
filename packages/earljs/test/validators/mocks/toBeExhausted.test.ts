f06b941b9e69475c2714f473cb56bbfe5d36288c
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2021-11-18 16:25:51 +0100

    Document toBeExhausted in mocks docs (#140)

--- packages/earljs/test/validators/mocks/toBeExhausted.test.ts
+++ packages/earljs/test/validators/mocks/toBeExhausted.test.ts
@@ -17,6 +17,22 @@ describe('toBeExhausted', () => {
 
       expect(() => earl(mock).toBeExhausted()).to.throw('Mock not exhausted!')
     })
+
+    it('works with multiple .given overrides in the queue', () => {
+      const square = mockFn<(x: number) => number>()
+        .given(2)
+        .returnsOnce(4)
+        .given(3)
+        .returnsOnce(9)
+        .given(4)
+        .returnsOnce(16)
+
+      expect(square(2)).to.eq(4)
+      expect(square(3)).to.eq(9)
+      expect(() => earl(square).toBeExhausted()).to.throw('Mock not exhausted!')
+      expect(square(4)).to.eq(16)
+      expect(() => earl(square).toBeExhausted()).not.to.throw()
+    })
   })
 
   describe('negated', () => {
