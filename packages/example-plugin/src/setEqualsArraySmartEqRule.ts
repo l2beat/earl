bf75f16dd7514791cc9eb8e6c4cd2719be4ccd64
hasparus <hasparus@gmail.com>
2021-12-02 22:06:29 +0100

    Validate args in setEqualsArraySmartEqRule

--- packages/example-plugin/src/setEqualsArraySmartEqRule.ts
+++ packages/example-plugin/src/setEqualsArraySmartEqRule.ts
@@ -1,7 +1,9 @@
 import { buildSmartEqResult, SmartEqRule } from 'earljs/internals'
 
 export const setEqualsArraySmartEqRule: SmartEqRule<Set<unknown>, unknown[]> = (actual, expected) => {
-  if (expected.every((x) => actual.has(x))) {
-    return buildSmartEqResult(true)
-  }
+  if (!(actual instanceof Set && expected instanceof Array)) return
+
+  const result = expected.every((x) => actual.has(x))
+
+  return buildSmartEqResult(result)
 }
