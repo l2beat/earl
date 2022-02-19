6c03fbaf0e57e8435321f8e5343710e2ef038634
andrew0 <739172+andrew0@users.noreply.github.com>
2022-02-19 13:12:49 -0500

    Capture file path from test definition callsite
    Prior to this commit the code was capturing the test file path when `test.run()`
    was called, but this is not necessarily going to be the same file as where the
    tests are defined. This change makes it so the test file path is captured where
    the test itself is defined.

--- packages/earljs/src/test-runners/uvu.ts
+++ packages/earljs/src/test-runners/uvu.ts
@@ -2,9 +2,19 @@ import debug from 'debug'
 import { Callback, Test } from 'uvu'
 
 import { setTestRunnerIntegration } from '../testRunnerCtx'
-import { TestRunnerCtx } from './TestRunnerCtx'
+import { TestInfo, TestRunnerCtx, TestRunnerHook } from './TestRunnerCtx'
+
+type Mutable<T> = { -readonly [P in keyof T]: T[P] }
 
 const d = debug('earljs:uvu')
+
+const beforeHooks: TestRunnerHook[] = []
+const afterHooks: TestRunnerHook[] = []
+const testRunnerCtx: TestRunnerCtx = {
+  testInfo: undefined as unknown as TestInfo,
+  beforeTestCase: (fn) => beforeHooks.push(fn),
+  afterTestCase: (fn) => afterHooks.push(fn),
+}
 
 function main() {
   if (typeof require !== 'function') {
@@ -13,59 +23,55 @@ function main() {
 
   d('earljs/uvu integration is being registered...')
 
-  const uvu: typeof import('uvu') = require('uvu')
-  patchTest(uvu.test)
+  setTestRunnerIntegration(testRunnerCtx)
 
+  const uvu = require('uvu') as Mutable<typeof import('uvu')>
   const suite = uvu.suite
-  uvu.suite = (...args) => {
-    const test = suite(...args)
-    patchTest(test)
-    return test
+
+  uvu.test = wrapTestSuite(uvu.test)
+  uvu.suite = (...args) => wrapTestSuite(suite(...args))
+}
+
+function wrapTestSuite<T>(test: Test<T>) {
+  const run = wrapTestRun(test, test.run)
+  return new Proxy(test, {
+    get: (target, prop, receiver) => (prop === 'run' ? run : Reflect.get(target, prop, receiver)),
+    apply: (target, thisArg, [name, testCase]: Parameters<Test>) => target.call(thisArg, name, wrapTestCase(testCase)),
+  })
+}
+
+function wrapTestRun<T>(test: Test<T>, run: Test<T>['run']): Test<T>['run'] {
+  return () => {
+    beforeHooks.forEach((hook) => test.before.each(hook as Callback))
+    afterHooks.forEach((hook) => test.after.each(hook as Callback))
+    return run.call(test)
   }
 }
 
-function patchTest(test: Test) {
-  const run = test.run.bind(test)
-  test.run = () => {
-    const testRunnerCtx: TestRunnerCtx = {
-      testInfo: {
-        suitName: [],
-        testName: '',
-        testFilePath: parseFilePathFromStackTrace(test.run),
-      },
-      beforeTestCase: (fn) => test.before.each(fn as Callback),
-      afterTestCase: (fn) => test.after.each(fn as Callback),
-    }
-
-    test.before(() => setTestRunnerIntegration(testRunnerCtx))
-
-    test.before.each((ctx) => {
-      testRunnerCtx.testInfo.suitName = ctx.__suite__ ? [ctx.__suite__] : []
-      testRunnerCtx.testInfo.testName = ctx.__test__
-    })
-
-    return run()
-  }
-}
-
-function parseFilePathFromStackTrace(ctor: Function) {
-  // Override the prepareStackTrace function to get access to the unformatted
-  // CallSite objects
+function wrapTestCase<T>(fn: Function): Callback<T> {
+  // Override the prepareStackTrace function to get access to the unformatted CallSite objects
   // https://v8.dev/docs/stack-trace-api
   const { prepareStackTrace } = Error
   Error.prepareStackTrace = (_err, stack) => stack
 
   const err: { stack: NodeJS.CallSite[] } = { stack: [] }
-  Error.captureStackTrace(err, ctor)
-  const filename = err.stack[0]?.getFileName()
+  Error.captureStackTrace(err, wrapTestCase)
+  const testFilePath = err.stack[1]?.getFileName()
 
   Error.prepareStackTrace = prepareStackTrace
 
-  if (!filename) {
+  if (!testFilePath) {
     throw new Error('Unable to determine test file path')
   }
 
-  return filename
+  return (ctx) => {
+    testRunnerCtx.testInfo = {
+      suitName: ctx.__suite__ ? [ctx.__suite__] : [],
+      testName: ctx.__test__,
+      testFilePath,
+    }
+    return fn(ctx)
+  }
 }
 
 main()
