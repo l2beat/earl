78ef362ca151ac909c6ad34e7625dc776fcb51a1
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2023-03-12 13:56:27 +0100

    Improve toEqual

--- packages/earljs/src/validators/toEqual.test.ts
+++ packages/earljs/src/validators/toEqual.test.ts
@@ -3,8 +3,8 @@ import { expect } from 'chai'
 import { expect as earl } from '../index'
 
 describe('toEqual', () => {
-  describe('not negated', () => {
-    it('works with complex object', () => {
+  describe('without .not', () => {
+    it('passes for matching complex object', () => {
       class B {
         constructor(readonly prop2: string) {}
       }
@@ -22,51 +22,90 @@ describe('toEqual', () => {
         },
       }
 
-      earl(actual).toEqual({
-        trimmed: true,
-        timestamp: earl.anything(),
-        name: earl.stringMatching('Duck'),
-        age: earl.a(Number),
-        nested: {
-          b: earl.a(B),
-          deep: earl.a(Object),
-        },
-      })
+      expect(() => {
+        earl(actual).toEqual({
+          trimmed: true,
+          timestamp: earl.anything(),
+          name: earl.stringMatching('Duck'),
+          age: earl.a(Number),
+          nested: {
+            b: earl.a(B),
+            deep: earl.a(Object),
+          },
+        })
+      }).not.to.throw()
     })
 
-    it('throws on mismatch', () => {
-      expect(() => earl(42).toEqual(420)).to.throw('42 not equal to 420')
+    it('fails on unequal primitives', () => {
+      expect(() => {
+        earl(42).toEqual(420)
+      }).to.throw('42 not equal to 420')
     })
 
-    it('throws on prototype mismatch with a reasonable error message', () => {
+    it('fails on unequal objects', () => {
+      expect(() => {
+        earl({
+          a: undefined,
+          b: true,
+        }).toEqual({ b: false } as any)
+      }).to.throw('{ a: undefined, b: true } not equal to { b: false }')
+    })
+
+    it('fails on prototype mismatch with a reasonable error message', () => {
       class Test {
         constructor(public readonly property: boolean) {}
       }
 
-      expect(() => earl(new Test(true)).toEqual({ property: true })).to.throw(
-        'Test { property: true } not equal to { property: true }',
-      )
-    })
-
-    describe('error messages', () => {
-      it('throws on mismatch', () => {
-        expect(() =>
-          earl({
-            a: undefined,
-            b: true,
-          }).toEqual({ b: false } as any),
-        ).to.throw('{ a: undefined, b: true } not equal to { b: false }')
-      })
+      expect(() => {
+        earl(new Test(true)).toEqual({ property: true })
+      }).to.throw('Test { property: true } not equal to { property: true }')
     })
   })
 
-  describe('negated', () => {
-    it('works', () => {
-      earl(5).not.toEqual(7)
+  describe('with .not', () => {
+    it('passes when values are not equal', () => {
+      expect(() => {
+        earl(5).not.toEqual(7)
+      }).not.to.throw()
     })
 
-    it('throws', () => {
-      expect(() => earl(5).not.toEqual(5)).to.throw('5 equal to 5')
+    it('fails when values are equal', () => {
+      expect(() => {
+        earl(5).not.toEqual(5)
+      }).to.throw('5 equal to 5')
+    })
+  })
+
+  describe('is type safe', () => {
+    it('works for a matching type', () => {
+      earl(5).not.toEqual(10)
+    })
+
+    it('errors for a mismatched type', () => {
+      // @ts-expect-error - type mismatch
+      earl('foo').not.toEqual(10)
+    })
+
+    it('works for union types', () => {
+      interface A {
+        type: 'A'
+        a: number
+      }
+
+      interface B {
+        type: 'B'
+        b: string
+      }
+
+      type Union = A | B
+
+      earl<Union>({ type: 'A', a: 5 }).not.toEqual({ type: 'B', b: 'bar' })
+    })
+
+    it('works for recursive types', () => {
+      type LinkedList<T> = [T] | [T, LinkedList<T>]
+      const x: LinkedList<number> = [1, [2]]
+      earl(x).not.toEqual([2, [3, [4]]])
     })
   })
 })
--- packages/earljs/src/validators/toEqual.ts
+++ packages/earljs/src/validators/toEqual.ts
@@ -14,24 +14,12 @@ registerValidator('toEqual', toEqual)
 export function toEqual<T>(control: Control<T>, expected: T) {
   const actualFmt = formatCompact(control.actual)
   const expectedFmt = formatCompact(expected)
-  const reason = `${actualFmt} not equal to ${expectedFmt}`
-  const negatedReason = `${actualFmt} equal to ${expectedFmt}`
 
-  if (!isEqual(control.actual, expected)) {
-    control.assert({
-      success: false,
-      reason,
-      negatedReason,
-      actual: format(control.actual, null),
-      expected: format(expected, control.actual),
-    })
-  } else {
-    control.assert({
-      success: true,
-      reason,
-      negatedReason,
-      actual: format(control.actual, null),
-      expected: format(expected, control.actual),
-    })
-  }
+  control.assert({
+    success: isEqual(control.actual, expected),
+    reason: `${actualFmt} not equal to ${expectedFmt}`,
+    negatedReason: `${actualFmt} equal to ${expectedFmt}`,
+    actual: format(control.actual, null),
+    expected: format(expected, control.actual),
+  })
 }
