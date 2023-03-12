ce5e7825029820e7edb2785b59d0c72b17f34eab
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2023-03-12 18:58:59 +0100

    Add check

---
+++ packages/earljs/src/matchers/check.test.ts
@@ -0,0 +1,38 @@
+import { expect } from 'chai'
+
+import { expect as earl } from '../index'
+import { testMatcher } from '../test/matchers'
+import { check } from './check'
+
+describe(check.name, () => {
+  it('is correctly formatted', () => {
+    expect(earl.check((x) => x !== undefined).toString()).to.equal(
+      'check(predicate)',
+    )
+  })
+
+  it('is type safe', () => {
+    earl(0).toEqual(earl.check((x) => x !== undefined))
+  })
+
+  testMatcher(
+    check((x) => x !== undefined),
+    [
+      null,
+      false,
+      NaN,
+      0,
+      -0,
+      BigInt(0),
+      '',
+      1,
+      4,
+      'foo',
+      true,
+      [],
+      {},
+      Symbol(),
+    ],
+    [undefined],
+  )
+})
---
+++ packages/earljs/src/matchers/check.ts
@@ -0,0 +1,18 @@
+import { registerMatcher } from '../expect'
+
+declare module '../expect' {
+  interface Matchers {
+    /**
+     * Matches values for which the predicate returns `true`.
+     *
+     * @param predicate - the function for checking values
+     */
+    check(predicate: (value: unknown) => boolean): any
+  }
+}
+
+registerMatcher('check', check, () => 'check(predicate)')
+
+export function check(predicate: (value: unknown) => boolean) {
+  return (value: unknown) => predicate(value)
+}
