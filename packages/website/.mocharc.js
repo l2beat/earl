c003a0ddffc47201da932a93cd950b486a6e98fe
Krzysztof Kaczor <krzkaczor@gmail.com>
2023-03-21 15:19:18 +0100

    Move files around

--- packages/website/.mocharc.js
+++ packages/website/.mocharc.js
@@ -1,6 +1,6 @@
 process.env.NODE_ENV = 'test'
 module.exports = {
-  spec: 'scripts/**/*.test.ts',
+  spec: 'generators/**/*.test.ts',
   require: 'ts-node/register/transpile-only',
   watchExtensions: 'ts',
   extension: 'ts',
