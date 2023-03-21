b1911abebb7d550ded911279c3921bef1d03f116
Kris Kaczor <krzkaczor@gmail.com>
2023-03-21 19:24:41 +0400

    Bring back api-reference-generator (#257)

---
+++ packages/website/generators/api-reference/generate.test.ts
@@ -0,0 +1,18 @@
+import { expect } from '@earljs/published'
+import { readFileSync } from 'fs'
+
+import { generateMarkdownForMethodDocumentation } from './generate'
+import { parseTsDocComment } from './tsdocs/parse'
+import { sampleMethodComment } from './tsdocs/parse.test'
+
+describe('generateMarkdownForMethodDocumentation', () => {
+  it('generates markdown for a comment with params and examples', async () => {
+    const result = generateMarkdownForMethodDocumentation(
+      parseTsDocComment(sampleMethodComment),
+    )
+
+    // @todo use snapshots here
+    const expected = readFileSync(require.resolve('./test/docs.txt'), 'utf8')
+    expect(result).toEqual(expected)
+  })
+})
---
+++ packages/website/generators/api-reference/index.ts
@@ -0,0 +1,37 @@
+import { writeFileSync } from 'fs'
+import path from 'path'
+
+import { generateApiReference } from './generateApiReference'
+
+const frontmatter = `\
+---
+title: API Reference
+editLink: false
+outline: deep
+---
+
+# {{ $frontmatter.title }}
+
+`
+async function main() {
+  const reference = await generateApiReference({
+    basePath: path.resolve(__dirname, '../../../earljs/dist'),
+    files: [
+      'Validators::validators/**/toBeA.d.ts', // replace with once more docs for validators are written: 'Validators:validators:validators/**/*.d.ts',
+      'Matchers:expect:matchers/**/*.d.ts',
+      'Mocks:mock:mocks/mockFn.d.ts,mocks/types/index.d.ts',
+    ],
+  })
+
+  const fullPage = frontmatter + reference
+
+  const outputPath = path.resolve(__dirname, '../../docs/api/api-reference.md')
+  writeFileSync(outputPath, fullPage)
+  console.log(`Wrote API reference to ${outputPath}`)
+}
+
+void main().catch((err) => {
+  // eslint-disable-next-line no-console
+  console.error(err)
+  process.exit(1)
+})
