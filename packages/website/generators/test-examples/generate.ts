2bc31e5c33de69cd39664aac6668fde1788d96da
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2024-04-21 18:28:13 +0200

    Fix preamble

--- packages/website/generators/test-examples/generate.ts
+++ packages/website/generators/test-examples/generate.ts
@@ -4,17 +4,12 @@ import { join } from 'node:path'
 import type { Example } from './types'
 
 export function generateTestFile(examples: Example[]): string {
-  // cut off first few lines of preamble as they are comments
   const preamble = readFileSync(
     join(__dirname, './chunks/preamble.ts'),
     'utf-8',
   )
-    .split('\n')
-    .slice(3)
-    .join('\n')
 
-  return `
-  ${preamble}
+  return `${preamble}
   
   import { expect, mockFn, mockObject } from 'earl'
   
